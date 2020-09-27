3ab9e8981f824d3965a5a91900331754e62828a9
Liam Darmody <liamdarmody@gmail.com>
2020-09-27 18:27:50 +0100

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ import { RiHeart2Line } from "react-icons/ri";
 const Footer = () => (
   <footer className="site-footer">
     <div className="container">
-      <p>Made with <span className="icon -love"><RiHeart2Line/>< in London.</p>
+      <p>Made with <span className="icon -love"><RiHeart2Line/> in London.</p>
     </div>
   </footer>
 )
